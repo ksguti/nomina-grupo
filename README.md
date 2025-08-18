161b4c24a867e8fc7e2abe5e5e49f1f0ca26f91d
Kevin Samuel Gutierrez Herrera <samig0279@gmail.com>
2025-08-18 18:05:37 -0500

    proyecto con /app y /db

---
+++ README.md
@@ -0,0 +1,8 @@
+#Despues de clonar el repositorio 
+# 1. clic en tools
+# 2. clic en NuGet Package Manager
+# 3. clic en Package Manager Console 
+# 4. pegar el siguiente comando para reinstalar paquete Roslyn:
+   Install-Package Microsoft.CodeDom.Providers.DotNetCompilerPlatform
+# 5. reconstriur el proyecto en Built/ReBuilt Solution
+# 6. Ya estas listo para ejecutar...
---
+++ app/AplicacionNomina/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//Importar librerias para conectar a SQL Server
+using System.Data;
+using System.Data.SqlClient;
+//Importar librerias para manejar funcionalidades
+
+namespace AplicacionNomina.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Autenticar()
+        {
+            return View();
+        }
+        public ActionResult Registrarse()
+        {
+            return View();
+        }
+    }
+}
